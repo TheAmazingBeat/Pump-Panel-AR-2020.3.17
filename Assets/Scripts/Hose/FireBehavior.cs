0773af1400415a3477afd1199172d3007eaf7ce2
Isaac Baldivas <ice.baldivas@gmail.com>
2022-02-16 11:40:22 -0600

    Ability to extinguis fire with regeneration

--- Assets/Scripts/Hose/FireBehavior.cs
+++ Assets/Scripts/Hose/FireBehavior.cs
@@ -1,31 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBehavior : MonoBehaviour
 {
+   #region Variables
    public float fireHealth = 50;
    public float maxFireHealth = 50;
    public float fireRegen = 5;
    public bool IsOnFire = true;
+   public int extinguishRate = 1;
 
    public ParticleSystem fire;
-   public List<ParticleCollisionEvent> collisionEvents;
+   private ParticleSystem.EmissionModule fireEmission;
+   #endregion
 
+   #region Unity Methods
    // Start is called before the first frame update
    void Start()
    {
       fire = GetComponent<ParticleSystem>();
-      collisionEvents = new List<ParticleCollisionEvent>();
+      fireEmission = fire.emission;
+      fireHealth = fireEmission.rateOverTime.constant;
+   }
+
+   private void Update()
+   {
+      if (fireHealth <= 0)
+      {
+         fire.Stop();
+         fireHealth = 0;
+      }
+      else
+      {
+         // Fire regeneration
+         while (fireHealth < 50)
+         {
+            fireHealth += fireRegen * Time.deltaTime;
+            fireEmission.rateOverTime = fireHealth;
+         }
+      }
    }
 
    private void OnParticleCollision(GameObject other)
    {
-      int numCollistionEvents = fire.GetCollisionEvents(other, collisionEvents);
-      if (numCollistionEvents <= 0) return;
-
-
-
-      Debug.Log(numCollistionEvents);
+      // If fire is wet, decrease fire emission
+      if (other.tag == "Water")
+      {
+         fireHealth -= extinguishRate;
+         fireEmission.rateOverTime = fireHealth;
+      }
    }
+   #endregion
+
+   #region Other Methods
+   private void Spread(){
+      // called if collision is not water
+   }
+   #endregion
+
 }
